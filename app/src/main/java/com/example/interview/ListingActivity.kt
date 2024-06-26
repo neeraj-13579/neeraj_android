1eee673a25c546224d235d207a86168526412b28
neeraj <neeraj.k@designingsolutions.co.in>
2024-06-26 13:25:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/interview/ListingActivity.kt
@@ -0,0 +1,12 @@
+
+package com.example.interview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listingactivity)
+    }
+}
